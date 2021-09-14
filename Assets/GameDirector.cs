64bc7261694522c77987967a8deb9e0c6d142b3f
kazuki <kazu03665@gmail.com>
2021-09-14 11:40:55 +0900

    test02確認用

--- Assets/GameDirector.cs
+++ Assets/GameDirector.cs
@@ -37,6 +37,7 @@ public class GameDirector : MonoBehaviour
             this.GameReferee.GetComponent<GameReferee>().playertrun = false;
         }
         Debug.Log("start");
+        Debug.Log("test02");
     }
 
     // Update is called once per frame
